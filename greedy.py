06701cbe84d5975cbba83c9a7b759b004e0db322
adib-enc <adib35785@gmail.com>
2021-11-19 21:47:32 +0700

    skeleton

--- greedy.py
+++ greedy.py
@@ -3,10 +3,13 @@ import pandas as pd
 # load
 
 # greed
-data = pd.read_csv("dummydataset_timecost_trade.csv", sep=";")
+df = pd.read_csv("dummydataset_timecost_trade.csv", sep=";")
 
-print(data)
+print(df)
 
 def greedy(data):
     for d in data:
-        if data
+        if True:
+            print("feasible")
+
+greedy(df)
